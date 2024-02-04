311a7dc50007a093b90e5003494e96092c99abd4
Kazi Vau <75408375+SilentVirtualCriminalGang@users.noreply.github.com>
2024-02-04 15:14:15 +0600

    kazi.py

--- zeroboy.py
+++ zeroboy.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python2
+p#!/usr/bin/python2
 #coding=utf-8
 
 
@@ -49,23 +49,23 @@ def jalan(z):
 ##### LOGO #####
 logo = """ -----------------------------•◈•
 (  __)\\ ____--------------_------------•◈•
-|__(~)    •||•THE - AAHIL -OFFICAL------•◈•
-|__\~~) •||•RANA - RAJPUT---------------•◈•
-|__(-----\  •◈•------BLACK-TIGER--------•◈•
+|__(~)    •||•THE - NOOB CODER ---------•◈•
+|__\~~) •||•-------MR.--*--KAZI --------•◈•
+|__(-----\  •◈•----CYBER GAMER----------•◈•
 |__~~~\ •◈•-----█-------⑦-------█------•◈•
 |__~~~\ •◈•-----█-------⑧-------█------•◈•
 |__~~~\ •◈•-----█-------⑥-------█------•◈•
 \033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;FlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1; Aahilrna4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/Therana/zero
+\033[1;96mAuthor  \033[1;93m: \033[1;92mKAZI ASHRAFUZZAMAN
+\033[1;96mInstagram \033[1;93m: \033[1;@its_me_kazi07
+\033[1;96mFacebook  \033[1;93m: \033[1; YOUR.DAD.HERE07
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/the-criminalhere
 \033[1;91m======================================="""
 
 def tik():
 	titik = ['.   ','..  ','... ']
 	for o in titik:
-		print("\r\033[1;96m[●] \x1b[1;93mSedang masuk \x1b[1;97m"+o),;sys.stdout.flush();time.sleep(1)
+		print("\r\033[1;96m[●] \x1b[1;93m------ \x1b[1;97m"+o),;sys.stdout.flush();time.sleep(1)
 
 
 back = 0
@@ -80,15 +80,15 @@ vuln = "\033[32mVuln"
 os.system("clear")
 print "\033[1;96m ============================================================="
 print  """\033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;92mFlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1;92m Aahilrana4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/Therana/zero
+\033[1;96mAuthor  \033[1;93m: \033[1;92mKAZI ASHRAFUZZAMAN
+\033[1;96mInstagram \033[1;93m: \033[1;92m@its_me_kazi07
+\033[1;96mFacebook  \033[1;93m: \033[1;92m YOUR.DAD.HERE07
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/the-criminalhere
 \033[1;91m======================================="""
 print " \x1b[1;93m============================================================="
 
-CorrectUsername = "rana"
-CorrectPassword = "rana"
+CorrectUsername = "kazi"
+CorrectPassword = "kazi"
 
 loop = 'true'
 while (loop == 'true'):
@@ -100,10 +100,10 @@ while (loop == 'true'):
             loop = 'false'
         else:
             print "Wrong Password"
-            os.system('xdg-open https://www.Youtube.com/UCsdJQbRf0xpvwaDu1rqgJuA')
+            os.system('xdg-open https://www.facebook.com/MR.ERROR.HERE')
     else:
         print "Wrong Username"
-        os.system('xdg-open https://www.Youtube.com/UCsdJQbRf0xpvwaDu1rqgJuA')
+        os.system('xdg-open https://www.facebook.com/MR.ERROR.HEREp')
 
 def login():
 	os.system('clear')
